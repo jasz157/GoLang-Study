4d5bf7ce7e364e463c4cbeb69ee77db629669bc4
Jonnie Amaral <78632690+jasz157@users.noreply.github.com>
2023-03-29 03:37:36 +0000

    Study :construction: testing modules with golang

---
+++ Curso/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Em go definimos se a func e publica para outros packages nomeando a função com letras maiusculas
+// e definimos como privada apenas para o package onde foi criado nomeando a função com minuscula
+func Escrevendo() {
+	fmt.Println("Escrevendo do Auxiliar")
+}
